1823392ffff0fb938625c34430c85044abbc4e96
Riley Bergamasco <rlbergamasco@gmail.com>
2022-04-20 18:49:58 -0400

    Theming and dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { Box } from '@mui/material';
+import { useEffect } from 'react';
+import { Box, createTheme, ThemeProvider, Typography } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { Nav } from 'components';
 import { HomePage, HistoryPage, ClosetPage, SettingsPage } from 'pages';
+import { useSelector } from 'react-redux';
+import { selectIsDark } from "appSlice";
 
 const ROUTES = [
   {
@@ -23,19 +26,53 @@ const ROUTES = [
 ];
 
 const App = () => {
+  const isDark = useSelector(selectIsDark);
+
+  let theme = createTheme({
+    palette: {
+      type: isDark ? 'dark' : 'light',
+      primary: {
+        main: isDark ? '#6D74BD' : '#3A418C',
+      },
+      background: {
+        default: isDark ? '#303030' : '#fafafa',
+      },
+      text: {
+        primary: isDark ? '#fff' : 'rgba(0, 0, 0, 0.87)',
+      },
+    },
+    typography: {
+      fontFamily: '"Lato", "Roboto", "Helvetica", "Arial", sans-serif',
+      h1: {
+        fontFamily: '"Source Serif Pro", "Noto Serif", serif',
+      },
+      h2: {
+        fontFamily: '"Source Serif Pro", "Noto Serif", serif',
+      },
+    },
+  })
+
   return (
-    <Box sx={{ minHieght: "100vh", width: "100vw" }}>
-      <Routes>
-        {ROUTES.map((route) => (
-          <Route
-            key={route.path}
-            exact={route.path === '/'}
-            {...route}
-          />
-        ))}
-      </Routes>
-      <Nav />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{
+        width: '100vw',
+        height: '100vh',
+        p: 3,
+        bgcolor: 'background.default',
+        color: 'text.primary',
+      }}>
+        <Routes>
+          {ROUTES.map((route) => (
+            <Route
+              key={route.path}
+              exact={route.path === '/'}
+              {...route}
+            />
+          ))}
+        </Routes>
+        <Nav />
+      </Box>
+    </ThemeProvider>
   );
 }
 
