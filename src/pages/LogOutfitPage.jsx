0f57be5bb4d06ec2a5bd7af0ed90abf1ec1f4c52
Riley Bergamasco <rlbergamasco@gmail.com>
2022-04-28 14:51:21 -0400

    Added header to log outfit page

--- src/pages/LogOutfitPage.jsx
+++ src/pages/LogOutfitPage.jsx
@@ -1,10 +1,17 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 const LogOutfitPage = () => {
     return (
-        <Typography>
-            Log Outfit
-        </Typography>
+        <Box>
+            <Box sx={{ position: 'fixed', width: '100vw', zIndex: 10, top: 0, left: 0, bgcolor: 'background.paper' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', p: 3, pb: 2 }}>
+                    <Typography variant="h1">Log an Outfit</Typography>
+                </Box>
+            </Box>
+            <Box sx={{ mt: 7 }}>
+                <Typography>Content Here</Typography>
+            </Box>
+        </Box>
     );
 };
 
