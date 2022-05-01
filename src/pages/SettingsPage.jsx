ed72c55ebb10f10b94ea7dc27200fd5c5986f118
Sam Shi <samrshi@outlook.com>
2022-05-01 16:56:14 -0400

    clean up settings design

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -9,13 +9,21 @@ const SettingsPage = () => {
 
     return (
         <React.Fragment>
-            <Typography>
-                Settings
-            </Typography>
-            <Switch checked={isDark} onChange={() => dispatch(toggleIsDark())} />
-            <Button href="/">
-                <Typography variant="h2" sx={{ textTransform: 'capitalize' }}>Log Out</Typography>
-            </Button>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <Typography variant='h1'>
+                    Settings
+                </Typography>
+
+                <div style={{ display: "flex", verticalAlign: "center", marginTop: "15px" }}>
+                    <Typography style={{ marginRight: "auto" }}>Toggle Dark Mode</Typography>
+
+                    <Switch checked={isDark} onChange={() => dispatch(toggleIsDark())} />
+                </div>
+
+                <Button href="/">
+                    <Typography variant="h2" sx={{ textTransform: 'capitalize' }}>Log Out</Typography>
+                </Button>
+            </div>
         </React.Fragment>
     );
 };
