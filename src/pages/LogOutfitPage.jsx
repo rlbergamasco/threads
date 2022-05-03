11fd8c9e48c0ac52700423aa9fd87aa35c5be357
Riley Bergamasco <rlbergamasco@gmail.com>
2022-05-03 02:19:50 -0400

    Connected date picker in log outfit

--- src/pages/LogOutfitPage.jsx
+++ src/pages/LogOutfitPage.jsx
@@ -5,7 +5,6 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectOutfits, selectItems, addOutfit } from "appSlice";
 import { Add, AddAPhoto } from '@mui/icons-material';
 import { OutfitCard, PhotoAPI, UploadImage } from 'components';
-import { HomePage } from './HomePage';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
@@ -50,7 +49,7 @@ const LogOutfitPage = () => {
             notes: notes,
             imageURL: imageURL,
             items: items,
-            date: currentDate
+            date: new Date(datePickerFormat).getTime()
         }
         ));
         navigate('/home');
@@ -68,11 +67,7 @@ const LogOutfitPage = () => {
                     </Box>
                 </Box>
 
-                <Box sx={{ mt: 7 }}>
-                    <Typography variant="h2">Date: {currentDate}</Typography>
-                </Box>
-
-                <Box sx={{ my: 3 }}>
+                <Box sx={{ mt: 7, mb: 3 }}>
                     <Typography variant="h2" sx={{ pb: 1 }}>Date</Typography>
                     <TextField
                         id="date"
